31260294237bfbb7e1258b3b47820f5177b6bba5
Mazedul Islam <mazidmailbox@gmail.com>
2023-01-19 20:22:21 +0600

    adding video streaming api endpoint

---
+++ src/pages/api/videos.ts
@@ -0,0 +1,56 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { createReadStream, statSync } from "fs";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+type ErrorResponse = {
+  error: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | ErrorResponse>
+) {
+  const { method } = req;
+
+  if (method === "GET") {
+    // Ensure there is a range given for the video
+    const range = req.headers.range;
+    if (!range) {
+      res.status(400).send({ error: "Requires Range header" });
+    }
+
+    // get video stats (about 61MB)
+    const videoPath = "../../videos/nature.mp4";
+    const videoSize = statSync(videoPath).size;
+
+    // Parse Range
+    // Example: "bytes=32324-"
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range?.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+    // Create headers
+    const contentLength = end - start + 1;
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "video/mp4",
+    };
+
+    // HTTP Status 206 for Partial Content
+    res.writeHead(206, headers);
+
+    // create video read stream for this particular chunk
+    const videoStream = createReadStream(videoPath, { start, end });
+
+    // Stream the video chunk to the client
+    videoStream.pipe(res);
+  }
+
+  return res.status(405).json({ error: `Method ${method} not allowed` });
+}
