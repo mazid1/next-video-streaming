87721d8e2342b880d0d32bb7cc2cf1aee00f8f74
Mazedul Islam <mazidmailbox@gmail.com>
2023-01-21 11:52:09 +0600

    render clickable link by VideoList component

--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -1,11 +1,18 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Button } from '@chakra-ui/react'
+import NextLink from 'next/link'
 
 type VideoListProps = {
   fileNames: string[]
 }
 
 function VideoList({ fileNames }: VideoListProps) {
-  const contents = fileNames.map((file) => <Box key={file}>{file}</Box>)
+  const contents = fileNames.map((fileName) => (
+    <Box key={fileName}>
+      <Button as={NextLink} href={`/videos/${fileName}`}>
+        {fileName}
+      </Button>
+    </Box>
+  ))
   return <>{contents}</>
 }
 
