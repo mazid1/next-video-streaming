ed455939d9811eb28d37b01f2cad7d83c489003e
Mazedul Islam <mazidmailbox@gmail.com>
2023-01-22 13:02:10 +0600

    logo and favicon added

---
+++ src/components/layout/Logo.tsx
@@ -0,0 +1,24 @@
+import { Heading, HStack } from '@chakra-ui/react'
+import Image from 'next/image'
+import NextLink from 'next/link'
+import { UrlObject } from 'url'
+import logo from '../../../public/images/logo65x65.png'
+
+function Logo() {
+  return (
+    <HStack wrap="nowrap">
+      <Image src={logo} alt="logo" height={40} />
+      <Heading>Videos</Heading>
+    </HStack>
+  )
+}
+
+export function LinkLogo({ href }: { href: string | UrlObject }) {
+  return (
+    <NextLink href={href}>
+      <Logo />
+    </NextLink>
+  )
+}
+
+export default Logo
--- src/components/layout/Toolbar.tsx
+++ src/components/layout/Toolbar.tsx
@@ -1,5 +1,12 @@
+import { HStack, Link } from '@chakra-ui/react'
+import { LinkLogo } from './Logo'
+
 function Toolbar() {
-  return <div>Toolbar</div>
+  return (
+    <HStack>
+      <Link href="/" as={LinkLogo} />
+    </HStack>
+  )
 }
 
 export default Toolbar
