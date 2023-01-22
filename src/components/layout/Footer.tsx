1df1253bdfe1cd72d5bddc686da6f2fd1740553f
Mazedul Islam <mazidmailbox@gmail.com>
2023-01-22 16:10:59 +0600

    footer updated

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,18 @@
+import { Box, Container, Text, useColorModeValue } from '@chakra-ui/react'
+
 function Footer() {
-  return <div>Footer</div>
+  return (
+    <Box
+      as="footer"
+      borderColor={useColorModeValue('gray.300', 'gray.600')}
+      borderTopWidth={1}
+      borderStyle="solid"
+    >
+      <Container maxW="6xl" py={4} centerContent>
+        <Text>Created with &hearts; by Mazid</Text>
+      </Container>
+    </Box>
+  )
 }
 
 export default Footer
