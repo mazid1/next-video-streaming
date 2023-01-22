7e9f8a6698dcadb2ce0f2a02972027c9e319f691
Mazedul Islam <mazidmailbox@gmail.com>
2023-01-22 15:08:27 +0600

    color mode switcher added

--- src/components/layout/Toolbar.tsx
+++ src/components/layout/Toolbar.tsx
@@ -1,10 +1,18 @@
-import { HStack, Link } from '@chakra-ui/react'
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+import { HStack, IconButton, Link, useColorMode } from '@chakra-ui/react'
 import { LinkLogo } from './Logo'
 
 function Toolbar() {
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
     <HStack>
       <Link href="/" as={LinkLogo} />
+      <IconButton
+        icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+        onClick={() => toggleColorMode()}
+        aria-label={'Toggle Dark Mode'}
+      />
     </HStack>
   )
 }
